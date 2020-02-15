e1d5484c0ae051529963a463915e782072d77add
Taj Sivers <tajsivers@Tajs-MacBook-Pro.local>
2020-02-15 10:22:48 -0500

    Updated Library

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 //Set up mongoose connection
 var mongoose = require('mongoose');
 // mongodb+srv://TajSivers:c0ldlike6@cluster0-uyjtg.azure.mongodb.net/test?retryWrites=true&w=majority
-var mongoDB = 'mongodb+srv://TajSivers:TajSivers@cluster0-uyjtg.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://TajSivers:TajSivers@cluster0-uyjtg.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
